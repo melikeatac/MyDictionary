7ab92d046c13f20024a29f9c79f748750dcd7344
melikeatac4 <melikeatac4@gmail.com>
2021-02-20 23:27:22 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] TempKeys = keys;//Önceki değerleri kaybetmemek için kullanılır.
+            V[] TempValues = values;//Önceki değerleri kaybetmemek için kullanılır.
+            keys = new K[keys.Length + 1];//dizinin eleman sayısı 1 arttırılır.
+            values = new V[values.Length + 1];//dizinin eleman sayısı 1 arttırılır.
+            for (int i = 0; i <TempKeys.Length; i++)
+            {
+                keys[i] = TempKeys[i];
+                values[i] = TempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary1 = new MyDictionary<string, string>();
+            myDictionary1.Add("Melike", "Ataç");
+            myDictionary1.Add("Beyza", "Yanar");
+            myDictionary1.Add("Dilan", "Çirkin");
+            myDictionary1.Add("Büşra", "Bozkuş");
+
+        }
+    }
+}
